7cf5355a376d7927915cac8dd0f28762ee2df99a
fmls03 <melisfrancesco.003@gmail.com>
2022-05-31 11:57:58 +0200

    iniziato pagina Signup

--- frontend/src/views/SignupPage.vue
+++ frontend/src/views/SignupPage.vue
@@ -1,7 +1,11 @@
 <template>
-    <v-card>
-        <v-text-field>Miao</v-text-field>
-    </v-card>
+    <div class="d-flex justify-center align-center" id="div">
+        <v-card class="d-flex flex-column justify-center align-center" width="500" height="500" >
+            <v-card-title class="mt-200px">Sign Up</v-card-title>
+
+            <v-text-field label="Username" v-model="username"></v-text-field>
+        </v-card>
+    </div>
 </template>
 
 
@@ -10,3 +14,10 @@ export default {
     name: 'SignupPage'
 }
 </script>
+
+
+<style>
+#div{
+    height: '100%'
+}
+</style>
